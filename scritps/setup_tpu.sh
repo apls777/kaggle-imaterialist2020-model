457e1f3aea86f3f82a47b3f99fe3d15a3aa839a3
hrsma2i <hrs.ma2i@gmail.com>
2021-04-19 14:53:43 +0900

    Downgrade TF version: 2.4.1 -> 1.15.5

--- scritps/setup_tpu.sh
+++ scritps/setup_tpu.sh
@@ -2,10 +2,12 @@
 
 PROJECT_ID=$1
 
+# Tensorflow version may be 1.13
+# c.f., https://github.com/apls777/kaggle-imaterialist2020-model/blob/7466434d719b346b04ea0cde8c121d45c1338ce1/tf_tpu_models/official/mask_rcnn/mask_rcnn_k8s.yaml#L25:w
 ctpu up --project="$PROJECT_ID" \
     --zone=us-central1-a \
-    --vm-only \
     --disk-size-gb=300 \
     --machine-type=n1-standard-8 \
     --name=kaggle-imat2020 \
-    --tf-version=2.4.1
+    --tf-version=1.15.5 \
+    --tpu-size=v3-8
