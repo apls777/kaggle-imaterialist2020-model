396a904af297d7357f8b4d6538ff3f5642faab84
hrsma2i <hrs.ma2i@gmail.com>
2021-05-17 13:20:39 +0000

    Clean train.sh up.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -eu
-export STORAGE_BUCKET=gs://strf-sandbox/hiroshi.matsui/segmentation/kaggle-imaterialist-2020
-export MODEL_DIR="$STORAGE_BUCKET"/models
+INPUT_GCS_PATTERN=$1
+# e.g., gs://tfrecords/train-*
+OUT_GCS_DIR=$2
+# e.g., gs://models/model_name
 
 PYTHONPATH=tf_tpu_models \
     python tf_tpu_models/official/detection/main.py \
-    --model="mask_rcnn" \
-    --model_dir="$MODEL_DIR" \
     --mode="train" \
+    --model="mask_rcnn" \
+    --model_dir="$OUT_GCS_DIR" \
     --eval_after_training=False \
     --config_file=configs/spinenet/sn143-imat-v2.yaml \
     --use_tpu=True \
-    --tpu="kaggle-imat2020"
+    --tpu="kaggle-imat2020" \
+    --params_override="train.train_file_pattern=$INPUT_GCS_PATTERN"
