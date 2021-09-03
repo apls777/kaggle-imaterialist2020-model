b2721fd7dfee09e3dfa2cb3d71ec42008916b470
light0w0 <58503997+light0w0@users.noreply.github.com>
2021-09-03 19:20:36 +0900

    [wip]->batch

--- cmd/segment_from_gcs_to_bq/__main__.py
+++ cmd/segment_from_gcs_to_bq/__main__.py
@@ -24,7 +24,7 @@ from __future__ import absolute_import, annotations, division, print_function
 
 import os
 from dataclasses import asdict, dataclass
-from typing import List, NewType
+from typing import NewType
 
 import numpy as np
 import six
@@ -33,7 +33,6 @@ from absl import flags, logging
 from configs import factory as config_factory
 from dataloader import mode_keys
 from evaluation import coco_utils
-from google.cloud import bigquery
 from hyperparameters import params_dict
 from modeling import factory as model_factory
 from typing_extensions import TypedDict
@@ -65,6 +64,7 @@ flags.DEFINE_string(
     "model", "mask_rcnn", "Support `retinanet`, `mask_rcnn` and `shapemask`."
 )
 flags.DEFINE_integer("image_size", 640, "The image size.")
+flags.DEFINE_integer("batch_size", 2, "The batch size.")
 flags.DEFINE_string("checkpoint_path", "", "The path to the checkpoint file.")
 flags.DEFINE_string("config_file", "", "The config file template.")
 flags.DEFINE_string(
@@ -76,26 +76,36 @@ flags.DEFINE_string(
 flags.DEFINE_string(
     "image_file_pattern", "", "The glob that specifies the image file pattern."
 )
-flags.DEFINE_string("project_id", "", "the ID of Google Cloud Storage Project")
-flags.DEFINE_string("result_table", "", "the ID of BigQuery table")
 flags.DEFINE_float(
     "min_score_threshold", 0.05, "The minimum score thresholds in order to draw boxes."
 )
 
 
-def insert_bq(
-    bq_client: bigquery.Client,
-    result_table: str,
-    rows_to_insert: List[Segmentation],
-) -> None:
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    image = tf.io.decode_image(image, channels=3)
+    image.set_shape([None, None, 3])
 
-    errors = bq_client.insert_rows_json(
-        result_table, rows_to_insert
-    )  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
+    image = input_utils.normalize_image(image)
+    image_size = [FLAGS.image_size, FLAGS.image_size]
+    image, image_info = input_utils.resize_and_crop_image(
+        image, image_size, image_size, aug_scale_min=1.0, aug_scale_max=1.0
+    )
+    image.set_shape([image_size[0], image_size[1], 3])
+
+    return image, image_info
+
+
+def input_fn(data_files_pattern, batch_size):
+    filenames = tf.io.gfile.glob(data_files_pattern)
+    dataset = tf.data.Dataset.from_tensor_slices(filenames)
+    dataset = dataset.map(
+        load_and_preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+
+    dataset = dataset.batch(batch_size, drop_remainder=False)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset
 
 
 def main(unused_argv):
@@ -117,35 +127,27 @@ def main(unused_argv):
     model = model_factory.model_generator(params)
 
     with tf.Graph().as_default():
-        image_input = tf.placeholder(shape=(), dtype=tf.string)
-        image = tf.io.decode_image(image_input, channels=3)
-        image.set_shape([None, None, 3])
-
-        image = input_utils.normalize_image(image)
-        image_size = [FLAGS.image_size, FLAGS.image_size]
-        image, image_info = input_utils.resize_and_crop_image(
-            image, image_size, image_size, aug_scale_min=1.0, aug_scale_max=1.0
+        images = tf.placeholder(
+            shape=(FLAGS.batch_size, FLAGS.image_size, FLAGS.image_size, 3),
+            dtype=tf.float32,
         )
-        image.set_shape([image_size[0], image_size[1], 3])
+        # images: (batch_size, width, height, RGB=3)
 
-        # batching.
-        images = tf.reshape(image, [1, image_size[0], image_size[1], 3])
-        # images: (batch_size=2, width, height, RGB=3)
-        images_info = tf.expand_dims(image_info, axis=0)
-        # image_info: (2, 4, 2)=(batch_size, original|desired|scale|offset, height(y)|width(x))  # noqa: E501
+        image_infos = tf.placeholder(shape=(FLAGS.batch_size, 4, 2), dtype=tf.float32)
+        # image_infos: (batch_size, original|desired|scale|offset=4, height(y)|width(x)=2)  # noqa: E501
 
         # model inference
         outputs = model.build_outputs(
             images,
-            {"image_info": images_info},
+            {"image_info": image_infos},
             mode=mode_keys.PREDICT,
         )
 
         outputs["detection_boxes"] = outputs["detection_boxes"] / tf.tile(
-            images_info[:, 2:3, :], [1, 1, 2]
+            image_infos[:, 2:3, :], [1, 1, 2]
         )
 
-        predictions = model.build_predictions(outputs, {"image_info": images_info})
+        predictions = model.build_predictions(outputs, {"image_info": image_infos})
 
         source_id = tf.placeholder(dtype=tf.int32)
         predictions["pred_source_id"] = tf.expand_dims(source_id, axis=0)
@@ -153,68 +155,92 @@ def main(unused_argv):
         # Create a saver in order to load the pre-trained checkpoint.
         saver = tf.train.Saver()
 
+        image_files_pattern = f"{FLAGS.image_file_pattern.rstrip('/')}/*"
+        print(image_files_pattern)
+        # label_to_index = dict(
+        #    (index, os.path.basename(name)) for index, name in enumerate(image_files)
+        # )
+        # print(label_to_index)
+        # for source_index, image_file in enumerate(image_files):
+
+        print(" - Processing image ...")
+
+        dataset = input_fn(
+            data_files_pattern=image_files_pattern, batch_size=FLAGS.batch_size
+        )
+        print(dataset)
+        itr = tf.data.make_one_shot_iterator(dataset)
+
+        print("**************")
+
         with tf.Session() as sess:
-
-            bq_client = bigquery.Client(project=FLAGS.project_id)
-
             print(" - Loading the checkpoint...")
             saver.restore(sess, FLAGS.checkpoint_path)
 
-            image_files = tf.gfile.Glob(f"{FLAGS.image_file_pattern.rstrip('/')}/*")
-            for source_index, image_file in enumerate(image_files):
+            while True:
+                try:
+                    image_batch, image_info_batch = itr.get_next()
+                    print("image_batch: ", image_batch)
+                    print("image_info_batch: ", image_info_batch)
 
-                print(f" - Processing image {source_index}...")
+                    image_batch_np = sess.run(image_batch)
+                    image_info_batch_np = sess.run(image_info_batch)
+                    predictions_np = sess.run(
+                        predictions,
+                        feed_dict={
+                            images: image_batch_np,
+                            image_infos: image_info_batch_np,
+                            source_id: [1, 2],
+                        },
+                    )
+                    print(" --- INFERENCE FINISHED")
+                    print(predictions_np)
 
-                with tf.gfile.GFile(image_file, "rb") as f:
-                    image_bytes = f.read()
+                    predictions_ = {}
+                    for key, val in predictions_np.items():
+                        if key[0:5] == "pred_":
+                            predictions_[key[5::]] = val
 
-                predictions_np = sess.run(
-                    predictions,
-                    feed_dict={image_input: image_bytes, source_id: source_index},
-                )
+                    for k, v in six.iteritems(predictions_):
+                        predictions_[k] = np.expand_dims(predictions_[k], axis=0)
 
-                predictions_ = {}
-                for key, val in predictions_np.items():
-                    if key[0:5] == "pred_":
-                        predictions_[key[5::]] = val
-
-                for k, v in six.iteritems(predictions_):
-                    predictions_[k] = np.expand_dims(predictions_[k], axis=0)
-
-                coco_annotations = coco_utils.convert_predictions_to_coco_annotations(
-                    predictions_,
-                    output_image_size=1024,
-                    score_threshold=FLAGS.min_score_threshold,
-                )
-
-                seg_list = []
-
-                for j in range(len(coco_annotations)):
-
-                    # In the case of byte type, it cannot be converted to json
-                    coco_annotations[j]["segmentation"]["counts"] = str(
-                        coco_annotations[j]["segmentation"]["counts"]
+                    coco_annotations = (
+                        coco_utils.convert_predictions_to_coco_annotations(
+                            predictions_,
+                            output_image_size=1024,
+                            score_threshold=FLAGS.min_score_threshold,
+                        )
                     )
 
-                    seg = Segmentation(
-                        image_id=coco_annotations[j]["image_id"],
-                        filename=os.path.basename(image_file),
-                        segmentation=coco_annotations[j]["segmentation"],
-                        imat_category_id=coco_annotations[j]["category_id"],
-                        score=coco_annotations[j]["score"],
-                        mask_mean_score=coco_annotations[j]["mask_mean_score"],
-                    )
+                    seg_list = []
+
+                    for j in range(len(coco_annotations)):
+
+                        # In the case of byte type, it cannot be converted to json
+                        coco_annotations[j]["segmentation"]["counts"] = str(
+                            coco_annotations[j]["segmentation"]["counts"]
+                        )
+
+                        seg = Segmentation(
+                            image_id=coco_annotations[j]["image_id"],
+                            filename=os.path.basename(image_files_pattern),
+                            segmentation=coco_annotations[j]["segmentation"],
+                            imat_category_id=coco_annotations[j]["category_id"],
+                            score=coco_annotations[j]["score"],
+                            mask_mean_score=coco_annotations[j]["mask_mean_score"],
+                        )
 
                     seg_list.append(asdict(seg))
 
-                insert_bq(bq_client, FLAGS.result_table, seg_list)
+                    print(seg_list)
+                except tf.errors.OutOfRangeError as e:
+                    print(e)
+                    break
 
 
 if __name__ == "__main__":
     flags.mark_flag_as_required("model")
     flags.mark_flag_as_required("checkpoint_path")
     flags.mark_flag_as_required("image_file_pattern")
-    flags.mark_flag_as_required("project_id")
-    flags.mark_flag_as_required("result_table")
     logging.set_verbosity(logging.INFO)
     tf.app.run(main)
